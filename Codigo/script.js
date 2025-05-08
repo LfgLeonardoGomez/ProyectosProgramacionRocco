6f3fc4109d00e76041bcbb2ceb72924cfccd83f7
Fausto Chirino <faustetichirino@gmail.com>
2025-05-08 13:12:32 -0300

    Subiendo front

--- Codigo/script.js
+++
@@ -1,6 +0,0 @@
-const botonMenu=document.querySelector('.boton-menu');
-const menuLateral=document.querySelector('.menu-lateral');
-
-botonMenu.addEventListener('click',()=>{
-    menuLateral.classList.toggle('activo');
-})
